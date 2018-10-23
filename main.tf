a6ee270a653ecb4ea3a8a45a18c7484fdda37acb
nag432 <nag432@users.noreply.github.com>
2018-10-23 15:32:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,8 @@ provider "aws" {
     secret_key = "${var.secret_key}"
     region = "${var.region}"
 }
-resource "aws_instance","Brokers" {
+resource "aws_instance"
+"Brokers" {
  subnet_id = "${var.subnet_id}"
  ami = "${var.ami}"
  instance_type = "${var.instance_type}"
